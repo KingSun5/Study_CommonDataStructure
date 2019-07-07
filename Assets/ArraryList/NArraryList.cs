63f7232b30ae30b9f402a0330f49ee7e5f73591d
KingSun5 <729916355@qq.com>
2019-07-07 22:14:05 +0800

    添加

---
+++ Assets/ArraryList/NArraryList.cs
@@ -0,0 +1,299 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 模拟ArraryList接口
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public interface INArraryList<T> where T : Object
+{
+	
+	LinkedNode<T> StartNode { get; set; } //起始数据
+	
+	int Count { get; set; } //数据长度
+
+	void Add(T obj); //增加数据
+	
+	void AddRange(ICollection collection);//添加一堆
+
+	void Remove(T obj); //删除数据
+
+	void RemoveAt(int index); //删除指定数据
+
+	void RemoveRange(int min, int max); //删除范围内的元素
+
+	bool Contains(T obj);//是否包含数据
+
+	void Insert(int index,T value);//插入元素
+
+	void Clear();//清空
+}
+
+
+/// <summary>
+/// time:2019/7/6
+/// author:Sun
+/// des:重写ArraryList
+///
+/// 特点：
+/// 1、长度不限
+/// 2、元素类型Object
+/// 
+/// github:https://github.com/KingSun5
+/// csdn:https://blog.csdn.net/Mr_Sun88
+/// </summary>
+public class NArraryList<T>: INArraryList<T> where T : Object
+{
+	public LinkedNode<T> StartNode { get; set; }
+	
+	public int Count { get; set; }
+	
+	/// <summary>
+	/// 想NarraryList内添加元素
+	/// </summary>
+	/// <param name="obj"></param>
+	public void Add(T obj)
+	{
+		if (StartNode==null)
+		{
+			StartNode = new LinkedNode<T>(obj);
+			Count++;
+			return;
+		}
+
+		LinkedNode<T> current = StartNode;
+		while (current.Next!=null)
+		{
+			current = current.Next;
+		}
+		current.Next = new LinkedNode<T>(obj);
+		Count++;
+	}
+
+	/// <summary>
+	/// 添加一个组的元素
+	/// </summary>
+	/// <param name="collection"></param>
+	/// <exception cref="NotImplementedException"></exception>
+	public void AddRange(ICollection collection)
+	{
+		LinkedNode<T> current = StartNode;
+		
+		while (current.Next!=null)
+		{
+			current = current.Next;
+		}
+
+		foreach (T child in collection)
+		{
+			current.Next = new LinkedNode<T>(child);
+			current = current.Next;
+		}
+
+		Count += collection.Count;
+	}
+
+	public void Remove(T obj)
+	{
+
+		if (!Contains(obj))
+		{
+			Debug.Log("not contain this obj！");
+			return;
+		}
+		
+		LinkedNode<T> current = StartNode;
+		
+		if (current.Data==obj)
+		{
+			Clear();
+			return;
+		}
+
+		int _lenght = 1;
+		bool _isExist = false;
+
+		while (_lenght<Count)
+		{
+			if (current.Next.Data==obj)
+			{
+				if (current.Next.Next != null)
+				{
+					current.Next = current.Next.Next;
+				}
+				else
+				{
+					current.Next = null;
+				}
+				break;
+			}
+			_lenght++;
+		}
+	}
+
+	public void RemoveAt(int index)
+	{
+		if (index>Count)
+		{
+			Debug.Log("out of range!");
+			return;
+		}
+
+		LinkedNode<T> current = StartNode;
+
+		if (index==1)
+		{
+			if (current.Next==null)
+			{
+				Clear();
+			}
+			else
+			{
+				StartNode = current.Next;
+			}
+			return;
+		}
+		int currentIndex = 1;
+
+		while (currentIndex<Count)
+		{
+			currentIndex++;
+			if (currentIndex==index)
+			{
+				if (current.Next != null && current.Next.Next!=null)
+				{
+					current.Next = current.Next.Next;
+				}
+				else
+				{
+					current.Next = null;
+				}
+			}
+		
+		}
+	}
+
+	public void RemoveRange(int min, int max)
+	{
+		if (min<1||max>Count||min>=max)
+		{
+			Debug.Log("out of range!");
+			return;
+		}
+
+		if (min==1)
+		{
+			int currentIndex = 0;
+			while (currentIndex<max)
+			{
+				StartNode = StartNode.Next;
+				currentIndex++;
+			}
+			return;
+		}
+		
+		LinkedNode<T> current = StartNode;
+		
+		int currentIndex2 = 1;
+		while (currentIndex2<max)
+		{
+			currentIndex2++;
+			if (currentIndex2>=min)
+			{
+				if (current != null && current.Next.Next!=null)
+				{
+					current.Next = current.Next.Next;
+				}
+				else
+				{
+					if (current != null) current.Next = null;
+				}
+			}
+			else
+			{
+				if (current != null) current = current.Next;
+			}
+		}
+
+	}
+
+	public bool Contains(T obj)
+	{
+		bool isExist = false;
+		LinkedNode<T> current = StartNode;
+		int currentIndex = 0;
+		while (currentIndex<Count)
+		{
+			currentIndex++;
+			if (current.Data==obj)
+			{
+				isExist = true;
+				break;
+			}
+			else
+			{
+				current = current.Next;
+				currentIndex++;
+			}
+		}
+		return isExist;
+	}
+
+	public void Insert(int index, T value)
+	{
+		if (index>Count)
+		{
+			Debug.Log("out of range!");
+			return;
+		}
+		LinkedNode<T> node = new LinkedNode<T>(value);
+		LinkedNode<T> current = StartNode;
+		if (index==1)
+		{
+			StartNode = node;
+			StartNode.Next = current;
+			return;
+		}
+		int currentIndex = 1;
+		while (true)
+		{
+			currentIndex++;
+			if (currentIndex==index)
+			{
+				node.Next = current.Next.Next;
+				current.Next = node;
+				break;
+			}
+			current = current.Next;
+		}
+		
+	}
+
+
+	public void Clear()
+	{
+		throw new System.NotImplementedException();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Base.cs
@@ -0,0 +1,174 @@
+﻿
+#region ------------------------固定长度的顺序存储结构-------------------------
+
+/// <summary>
+/// 固定长度的顺序存储结构
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SeqStructure<T>
+{
+    private T[] _data;
+    private int _i;
+
+    public SeqStructure(int size)
+    {
+        _data = new T[size];
+    }
+
+    public void AddData(T var)
+    {
+        _data[_i++] = var;
+    }
+}
+
+#endregion -----------------------------------------------------------------
+
+#region ----------------------------单向链式结构-----------------------------
+
+
+/// <summary>
+/// 单向链式结构基础元素
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class LinkedNode<T>
+{
+    private T _data;
+
+
+    private LinkedNode<T> _next;
+
+    public LinkedNode()
+    {
+        _data = default(T);
+        _next = null;
+    }
+
+    public LinkedNode(T val)
+    {
+        _data = val;
+        _next = null;
+    }
+
+    public T Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+    public LinkedNode<T> Next
+    {
+        get { return _next; }
+        set { _next = value; }
+    }
+}
+
+class LinkedStructure<T>
+{
+    private LinkedNode<T> _first;
+    private LinkedNode<T> _current;
+
+    public LinkedStructure()
+    {
+        _first = null;
+    }
+
+    public void AddData(LinkedNode<T> var)
+    {
+        if (_first==null)
+        {
+            _first = var;
+            _current = var;
+        }
+        else
+        {
+            _current.Next = var;
+            _current = var;
+        }
+    }
+}
+
+
+#endregion -----------------------------------------------------------------
+
+#region ----------------------------双向链式结构-----------------------------
+
+public class DbNode<T>
+{
+    private T _data;//数据
+
+    private DbNode<T> _prev;//前驱引用域
+    private DbNode<T> _next;//后驱引用域
+
+    public DbNode(T val, DbNode<T> p)
+    {
+        _data = val;
+        _next = p;
+    }
+
+    public DbNode(DbNode<T> p)
+    {
+        _next = p;
+    }
+
+    public DbNode(T val)
+    {
+        _data = val;
+        _next = null;
+    }
+
+    public DbNode()
+    {
+        _data = default(T);
+        _next = null;
+    }
+
+    public T Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+
+    public DbNode<T> Prev
+    {
+        get { return _prev; }
+        set { _prev = value; }
+    }
+
+    public DbNode<T> Next
+    {
+        get { return _next; }
+        set { _next = value; }
+    }
+}
+
+
+
+#endregion -----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
